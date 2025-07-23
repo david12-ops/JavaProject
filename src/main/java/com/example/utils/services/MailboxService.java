1a93edc8e4f678f2477fff83c964233c8058a836
david12-ops <77929483+david12-ops@users.noreply.github.com>
2025-07-23 17:56:39 +0200

    added/updated functions

--- src/main/java/com/example/utils/services/MailboxService.java
+++ src/main/java/com/example/utils/services/MailboxService.java
@@ -1,7 +1,11 @@
 package com.example.utils.services;
 
 import java.io.File;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import com.example.dto.MessageDTO;
 import com.example.dto.UserDTO;
@@ -10,6 +14,8 @@ import com.example.model.repository.MessageRepository;
 import com.example.utils.RepositoryFactory;
 import com.example.utils.ValidationContext;
 import com.example.utils.enums.ValidationMode;
+import com.example.utils.enums.ViewLevel;
+import com.example.utils.enums.MessageStatus;
 import com.example.utils.interfaces.ErrorHandler;
 import com.example.utils.interfaces.MailService;
 import com.example.utils.interfaces.MessageValidator;
@@ -25,7 +31,13 @@ public class MailboxService implements MailService {
     private record LabeledValue(String label, Object value) {
     }
 
-    // Support Methods
+    private MessageDTO createMessageDTO(String messageId, String senderId, String senderMailAccount, String recevierId,
+            String recevierMailAccount, String subject, String message, LocalDateTime timestamp,
+            List<String> attachedBase64Files, Map<String, Set<MessageStatus>> statuses, List<File> attachedFiles) {
+        return new MessageDTO(messageId, senderId, senderMailAccount, recevierId, recevierMailAccount, subject, message,
+                timestamp, attachedBase64Files, statuses, attachedFiles);
+    }
+
     private boolean containsDataNull(String errorKey, LabeledValue... labeledValues) {
         for (LabeledValue labeledValue : labeledValues) {
             if (labeledValue.value == null) {
@@ -36,11 +48,27 @@ public class MailboxService implements MailService {
         return false;
     }
 
-    private String resolveUserId() {
+    private String resolveUserIdByEmail(String email) {
+        if (userDTOs == null || userDTOs.size() == 0)
+            return null;
+
+        for (UserDTO userDTO : userDTOs) {
+            String userEmailFromDTO = userDTO.getMailAccount();
+            if (userEmailFromDTO != null && userEmailFromDTO.equals(email))
+                return userDTO.getUserId();
+        }
         return null;
     }
 
-    private String resolveMessageId() {
+    private String resolveEmailByUserId(String userId) {
+        if (userDTOs == null || userDTOs.size() == 0)
+            return null;
+
+        for (UserDTO userDTO : userDTOs) {
+            String userIdFromDTO = userDTO.getUserId();
+            if (userIdFromDTO != null && userIdFromDTO.equals(userId))
+                return userDTO.getMailAccount();
+        }
         return null;
     }
 
@@ -50,10 +78,20 @@ public class MailboxService implements MailService {
     }
 
     @Override
-    public void sendMessage(UserToken senderToken, String recevierId, String subject, String message,
+    public void sendMessage(UserToken senderToken, String recevierEmail, String subject, String message,
             List<File> files) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'sendMessage'");
+        if (containsDataNull("sendMessage", new LabeledValue("token", senderToken)))
+            return;
+
+        boolean isValid = messageValidator.validMessageData(recevierEmail, subject, message)
+                && messageValidator.validFiles(files);
+        String resolvedRecevierId = resolveUserIdByEmail(recevierEmail);
+
+        if (isValid && !containsDataNull("sendMessage", new LabeledValue("recevierId", resolvedRecevierId))) {
+            MessageDTO newMessageDTO = createMessageDTO(null, senderToken.getUserId(), null, resolvedRecevierId, null,
+                    subject, message, LocalDateTime.now(), null, null, files);
+            messageRepository.addMessage(senderToken, newMessageDTO);
+        }
     }
 
     @Override
@@ -64,8 +102,33 @@ public class MailboxService implements MailService {
 
     @Override
     public void removeMessage(MessageDTO messageDTO) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'removeMessage'");
+        messageRepository.removeMessage(messageDTO);
+    }
+
+    @Override
+    public List<MessageDTO> getMessageDTOs(UserToken userToken, MessageStatus messageStatus) {
+        List<MessageDTO> messageDTOs = messageRepository.getAllMessageDtos();
+        List<MessageDTO> updatedMessageDTOs = new ArrayList<>();
+
+        boolean containsNull = containsDataNull("getMessageDTOs", new LabeledValue("userToken", userToken),
+                new LabeledValue("DTOs", messageDTOs));
+
+        if (containsNull)
+            return List.of();
+
+        messageDTOs.forEach(messageDTO -> {
+            String senderMailAccount = resolveEmailByUserId(messageDTO.getSenderId());
+            String recevierMailAccount = resolveEmailByUserId(messageDTO.getRecevierId());
+
+            MessageDTO updatedMessageDTO = createMessageDTO(messageDTO.getMessageId(), messageDTO.getSenderId(),
+                    senderMailAccount, messageDTO.getRecevierId(), recevierMailAccount, messageDTO.getSubject(),
+                    messageDTO.getMessage(), messageDTO.getTimestamp(), messageDTO.getAttachedBase64Files(),
+                    messageDTO.getStatuses(), messageDTO.getAttachedFiles());
+            updatedMessageDTO.sanitize(ViewLevel.PUBLIC);
+            updatedMessageDTOs.add(updatedMessageDTO);
+        });
+
+        return updatedMessageDTOs;
     }
 
     @Override
