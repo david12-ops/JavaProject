fedd18f72ce1cc1475ca91842f28c80906a11e31
david12-ops <77929483+david12-ops@users.noreply.github.com>
2025-07-23 17:56:08 +0200

    added new methods

--- src/main/java/com/example/utils/interfaces/MessageValidator.java
+++ src/main/java/com/example/utils/interfaces/MessageValidator.java
@@ -2,9 +2,19 @@ package com.example.utils.interfaces;
 
 import java.io.File;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import com.example.model.Message;
+import com.example.utils.enums.MessageStatus;
 
 public interface MessageValidator {
     boolean validFiles(List<File> files);
 
     boolean validMessageData(String whom, String subject, String message);
+
+    boolean containsOnlySupportedStatuses(Map<String, Set<MessageStatus>> messageStatuses,
+            List<MessageStatus> expectedMessageStatus);
+
+    boolean isStatusUpdateAllowed(Message message, MessageStatus newStatus, String userKey);
 }
