c1f8238dd06cf2f155e965810ecc284a01bd3b85
david12-ops <77929483+david12-ops@users.noreply.github.com>
2025-08-07 01:21:40 +0200

    interface update

--- src/main/java/com/example/utils/interfaces/MailService.java
+++ src/main/java/com/example/utils/interfaces/MailService.java
@@ -7,11 +7,12 @@ import java.util.List;
 import com.example.dto.MessageDTO;
 import com.example.model.UserToken;
 import com.example.utils.enums.MessageStatus;
+import com.example.utils.enums.OperationType;
 
 public interface MailService {
     void sendMessage(UserToken userToken, String recevierId, String subject, String message, List<File> files);
 
-    void updateStatus(UserToken userToken, MessageDTO messageDTO, MessageStatus newStatus);
+    void updateStatus(UserToken userToken, MessageDTO messageDTO, MessageStatus status, OperationType operationType);
 
     void removeMessage(UserToken userToken, MessageDTO messageDTO);
 
