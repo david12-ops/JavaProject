d2fc875ee9f98e2d2539eeb4d934a039d0e94d5f
david12-ops <77929483+david12-ops@users.noreply.github.com>
2025-08-08 17:51:49 +0200

    new test initialization

---
+++ src/test/java/com/example/MailboxServiceTest.java
@@ -0,0 +1,9 @@
+package com.example;
+
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(MockitoExtension.class)
+public class MailboxServiceTest {
+
+}
