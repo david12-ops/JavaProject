bc0973f9f1ceecaf286c676f87f50ae95350b808
david12-ops <77929483+david12-ops@users.noreply.github.com>
2025-08-09 23:33:45 +0200

    test function added

--- src/test/java/com/example/UserAccountServiceTest.java
+++ src/test/java/com/example/UserAccountServiceTest.java
@@ -1,9 +1,46 @@
 package com.example;
 
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.example.utils.interfaces.ErrorHandler;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith(MockitoExtension.class)
 public class UserAccountServiceTest {
+    private void compareErrors(String expectedMessage, String key, ErrorHandler errorHandler) {
+        String errorMessage = errorHandler.getError(key);
+        if (errorMessage != null) {
+            assertEquals(expectedMessage, errorMessage, "Mismatch in error message for key " + key);
+            errorHandler.removeError(key);
+        }
+    }
 
+    @Test
+    @DisplayName("Should remove account")
+    public void testRemoveAccount() {
+
+    }
+
+    @Test
+    @DisplayName("Should update image profile of account")
+    public void testUpdateImageProfile() {
+
+    }
+
+    @Test
+    @DisplayName("Should get all user accounts")
+    public void testGetAllUserAccounts() {
+
+    }
+
+    @Test
+    @DisplayName("Should get image profile of account")
+    public void testGetImageProfile() {
+
+    }
 }
--- src/test/java/com/example/UserAuthServiceTest.java
+++ src/test/java/com/example/UserAuthServiceTest.java
@@ -1,9 +1,52 @@
 package com.example;
 
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.example.utils.interfaces.ErrorHandler;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith(MockitoExtension.class)
 public class UserAuthServiceTest {
+    private void compareErrors(String expectedMessage, String key, ErrorHandler errorHandler) {
+        String errorMessage = errorHandler.getError(key);
+        if (errorMessage != null) {
+            assertEquals(expectedMessage, errorMessage, "Mismatch in error message for key " + key);
+            errorHandler.removeError(key);
+        }
+    }
 
+    @Test
+    @DisplayName("Should register new account")
+    public void testRegister() {
+
+    }
+
+    @Test
+    @DisplayName("Should login account")
+    public void testLogin() {
+
+    }
+
+    @Test
+    @DisplayName("Should update not logged account")
+    public void testUpdateNotLoggedAccount() {
+
+    }
+
+    @Test
+    @DisplayName("Should switch account")
+    public void testSwitchAccount() {
+
+    }
+
+    @Test
+    @DisplayName("Should update logged account")
+    public void testUpdateLoggedInAccount() {
+
+    }
 }
